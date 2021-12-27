c28ac6430e429541668fda2e4caee379fed3dd7b
SayeedMahdi <84064542+SayeedMahdi@users.noreply.github.com>
2021-12-27 14:15:22 +0430

    bug fixing

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const initWebRoutes = require("./routes/web");
-const  configViewEngine =require("./config/viewEngine");
 require("dotenv").config();
 const express = require("express");
 const cookie_parser =require("cookie-parser");
 const connect_flash=require("connect-flash");
-
+const path = require('path')
 const configSession =require("./config/session")
 const passport = require("passport");
 
 let app = express();
 
-
+app.use('/', express.static('public'))
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views')); 
 //config url 
 app.use(express.urlencoded({extended:false}));
 //config cookie parser
@@ -24,7 +25,6 @@ configSession (app);
 app.use(passport.initialize());
 app.use(passport.session());
 //config view Engine
-configViewEngine(app);
 
 //init all web routes
 app.use("/api",initWebRoutes)
