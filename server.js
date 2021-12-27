e022dbfa9a37917955ed7431ea8de96419850678
SayeedMahdi <84064542+SayeedMahdi@users.noreply.github.com>
2021-12-27 14:34:03 +0430

    fix styles

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const configSession =require("./config/session")
 const PORT = process.env.PORT || 8000;
 let app = express();
 
+app.use(express.static('public'))
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views')); 
 //config url 
